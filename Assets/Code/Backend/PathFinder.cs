80ae43d66bd63bad3cfca1e0a38b5e818504a90e
Smitha6 <smitha6@g.cofc.edu>
2018-06-15 09:44:34 -0400

    First Commit

---
+++ Assets/Code/Backend/PathFinder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour {
+
+    public const int MATRIX_ROWS = 6;  //number of nodes
+    public const int MATRIX_COLUMNS = MATRIX_ROWS;
+    public int[,] matrix = new int[MATRIX_ROWS, MATRIX_COLUMNS];
+
+    // Use this for initialization
+    void Start () {
+
+        
+        
+        int ran;
+
+        //fills out matrix.  1 == path  0 == no path
+        for (int i = 0; i < MATRIX_ROWS; i++){
+            for (int j = 0; j < MATRIX_COLUMNS; j++){
+
+                //licklihood of there being a connection bewteen 2 nodes
+                ran = Random.Range(0, 2);  //(wont ever use max num, 50-50 == 0->2)
+                if (ran == 0){
+                    matrix[i,j] = 1;
+                }
+                else{
+                    matrix[i, j] = 0;
+                }
+                
+            }
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Code/Frontend/Draw.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw : MonoBehaviour {
+
+    Vector2 targetPos;
+    //starting positions of top left node
+    private static int startX = -6;
+    private static int startY = -3;
+
+    private int xPos = startX;
+    private int yPos = startY;
+    private int spacing = 2;
+
+    // Use this for initialization
+    void Start () {
+        GameObject node = new GameObject();
+        SpriteRenderer renderer = node.AddComponent<SpriteRenderer>();
+        renderer.sprite = Resources.Load("Sprites/node_sprite", typeof(Sprite)) as Sprite;
+
+
+        int rows = PathFinder.MATRIX_ROWS / 2;
+        int cols = PathFinder.MATRIX_ROWS / 2;
+
+        //draws grid
+        for (int i = 0; i < rows; i++) {
+            changePos(node, startX, yPos);
+
+            xPos = startX;
+            for (int j = 0; j < cols - 1; j++) {
+                xPos += spacing;
+                Instantiate(node);
+                changePos(node, xPos, yPos);
+            }
+            Instantiate(node);
+            yPos += spacing;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    //changes the position of an object
+    void changePos(GameObject obj, int x, int y){
+        targetPos = obj.transform.position;
+        targetPos.x = x;
+        targetPos.y = y;
+        obj.transform.position = targetPos;
+    }
+}
