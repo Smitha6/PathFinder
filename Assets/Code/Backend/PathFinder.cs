f8f290fd901f126eefbe6897a9e0b6327b2dba2d
Smitha6 <smitha6@g.cofc.edu>
2018-06-16 08:02:05 -0400

    Initial commit

--- Assets/Code/Backend/PathFinder.cs
+++ Assets/Code/Backend/PathFinder.cs
@@ -4,37 +4,72 @@ using UnityEngine;
 
 public class PathFinder : MonoBehaviour {
 
-    public const int MATRIX_ROWS = 6;  //number of nodes
+    public const int MATRIX_ROWS = 25;  //number of nodes (4)
     public const int MATRIX_COLUMNS = MATRIX_ROWS;
-    public int[,] matrix = new int[MATRIX_ROWS, MATRIX_COLUMNS];
+    public static int[,] matrix = new int[MATRIX_ROWS, MATRIX_COLUMNS];
+    private int ran;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
-        
-        
-        int ran;
+    }
+
+    //first to run
+    void Awake() {
+
+
+
+        int MATRIX_ROWS = Random.Range(10, 30);
+
 
         //fills out matrix.  1 == path  0 == no path
         for (int i = 0; i < MATRIX_ROWS; i++){
             for (int j = 0; j < MATRIX_COLUMNS; j++){
 
                 //licklihood of there being a connection bewteen 2 nodes
-                ran = Random.Range(0, 2);  //(wont ever use max num, 50-50 == 0->2)
+                //will alwasy have 2 way connections
+                ran = Random.Range(0, 20);  //(wont ever use max num, 50-50 == 0->2)
                 if (ran == 0){
-                    matrix[i,j] = 1;
+                    matrix[i, j] = 1;
+                    matrix[j, i] = 1;
                 }
                 else{
                     matrix[i, j] = 0;
+                    matrix[j, i] = 0;
                 }
                 
             }
         }
         
+        /*
+        matrix[0, 0] = 0;
+        matrix[0, 1] = 1;
+        matrix[0, 2] = 1;
+        matrix[0, 3] = 1;
+
+        matrix[1, 0] = 1;
+        matrix[1, 1] = 0;
+        matrix[1, 2] = 1;
+        matrix[1, 3] = 1;
+
+        matrix[2, 0] = 1;
+        matrix[2, 1] = 1;
+        matrix[2, 2] = 0;
+        matrix[2, 3] = 1;
+
+        matrix[3, 0] = 1;
+        matrix[3, 1] = 1;
+        matrix[3, 2] = 1;
+        matrix[3, 3] = 0;
+        */
+
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
 }
--- Assets/Code/Frontend/Draw.cs
+++ Assets/Code/Frontend/Draw.cs
@@ -7,49 +7,204 @@ public class Draw : MonoBehaviour {
     Vector2 targetPos;
     //starting positions of top left node
     private static int startX = -6;
-    private static int startY = -3;
+    private static int startY = 3;
+
+    private int wrapPos = 5; //0 - 8
 
     private int xPos = startX;
     private int yPos = startY;
     private int spacing = 2;
 
+    private Color nodeColor = new Color32(144, 104, 190, 255);
+    private Color lineCollor = new Color32(110, 211, 207, 255);
+    private Color borderColor = new Color32(230, 39, 57, 255);
+
+    public GameObject startNode;
+    public GameObject endNode;
+
+    public AudioSource click;
+    
+
     // Use this for initialization
     void Start () {
-        GameObject node = new GameObject();
-        SpriteRenderer renderer = node.AddComponent<SpriteRenderer>();
-        renderer.sprite = Resources.Load("Sprites/node_sprite", typeof(Sprite)) as Sprite;
 
+        
 
-        int rows = PathFinder.MATRIX_ROWS / 2;
-        int cols = PathFinder.MATRIX_ROWS / 2;
-
-        //draws grid
-        for (int i = 0; i < rows; i++) {
-            changePos(node, startX, yPos);
-
-            xPos = startX;
-            for (int j = 0; j < cols - 1; j++) {
-                xPos += spacing;
-                Instantiate(node);
-                changePos(node, xPos, yPos);
-            }
-            Instantiate(node);
-            yPos += spacing;
-        }
-
-
+        DrawGrid();
+        DrawConnections();
+        RemoveHangingNodes();
+        
     }
 
     // Update is called once per frame
     void Update () {
-		
-	}
 
-    //changes the position of an object
-    void changePos(GameObject obj, int x, int y){
+        
+
+        //gets node under mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 origin = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                                         Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.zero, 0f);
+            //get start node
+            if (hit && startNode == null) {
+                Debug.Log("IN1");
+
+                startNode = hit.transform.gameObject;
+
+                GameObject border = new GameObject();
+                SpriteRenderer renderer = border.AddComponent<SpriteRenderer>();
+                renderer.color = borderColor;
+                renderer.sprite = Resources.Load("Sprites/node_sprite_border", typeof(Sprite)) as Sprite;
+                renderer.sortingOrder = 0;
+
+                border.transform.position = startNode.transform.position;
+
+                click = GetComponent<AudioSource>();
+                click.Play();
+            }
+            //get end node
+            else if (startNode != null && endNode == null){
+
+                endNode = hit.transform.gameObject;
+
+                GameObject border = new GameObject();
+                SpriteRenderer renderer = border.AddComponent<SpriteRenderer>();
+                renderer.color = borderColor;
+                renderer.sprite = Resources.Load("Sprites/node_sprite_border", typeof(Sprite)) as Sprite;
+                renderer.sortingOrder = 0;
+
+                border.transform.position = endNode.transform.position;
+
+                click = GetComponent<AudioSource>();
+                click.Play();
+
+                //----RUN PATH FINDER HERE----
+            }
+                
+        }
+    }
+
+
+    //changes the position of a node
+    void ChangePos(GameObject obj, int x, int y){
         targetPos = obj.transform.position;
         targetPos.x = x;
         targetPos.y = y;
         obj.transform.position = targetPos;
     }
+
+    void DrawGrid() {
+        int numNodes = PathFinder.matrix.GetLength(0); //1 for cols
+
+        GameObject node = new GameObject();
+        BoxCollider2D boxCollider2D = node.AddComponent<BoxCollider2D>();
+        SpriteRenderer renderer = node.AddComponent<SpriteRenderer>();
+        renderer.color = nodeColor;
+        renderer.sortingOrder = 1;
+        renderer.sprite = Resources.Load("Sprites/node_sprite", typeof(Sprite)) as Sprite;
+        
+        for (int i = 0; i < numNodes; i++) {
+            ChangePos(node, xPos, yPos);
+            node.name = i + "";
+
+            if (xPos > wrapPos)
+            {
+                xPos = startX;
+                yPos -= spacing;
+            }
+            else
+            {
+                xPos += spacing;
+            }
+            
+            if (i < numNodes - 1)
+                Instantiate(node);
+        }
+        
+    }
+
+    void DrawLine(Vector2 start, Vector2 end, string name)
+    {
+        GameObject line = new GameObject();
+        line.transform.position = start;
+        line.AddComponent<LineRenderer>();
+        line.name = name;
+        LineRenderer lr = line.GetComponent<LineRenderer>();
+        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        lr.SetColors(lineCollor, lineCollor);
+        lr.SetWidth(0.1f, 0.1f);
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+    }
+
+    void DrawConnections() {
+
+        int numNodes = PathFinder.MATRIX_ROWS;
+        GameObject nodeStart;
+        GameObject nodeEnd;
+
+        for (int i = 0; i < numNodes; i++) {
+
+            nodeStart = GetNode(i);
+
+            for (int j = 0; j < PathFinder.MATRIX_COLUMNS; j++) {
+
+                if (PathFinder.matrix[i, j] == 1) {
+                    
+                    nodeEnd = GetNode(j);
+
+                    Vector2 start = nodeStart.transform.position;
+                    Vector2 end = nodeEnd.transform.position;
+                    DrawLine(start, end, j + "");  //connection is named after the node it points to
+                }
+                
+            }
+        }
+        
+
+    }
+
+    void RemoveHangingNodes(){
+        int numCon = 0;
+        GameObject node;
+
+        for(int i = 0; i < PathFinder.MATRIX_ROWS; i++) {
+
+            for (int j = 0; j < PathFinder.MATRIX_COLUMNS; j++) {
+
+                if(PathFinder.matrix[i, j] == 1){
+                    numCon++;
+                }
+
+            }
+
+            if(numCon == 0){
+                node = GetNode(i);
+                Destroy(node);
+            }
+            numCon = 0;
+
+        }
+    }
+
+    public GameObject GetNode(int name) {
+        GameObject node;
+        string sName = name + "";
+
+
+        //used because one node wont be a clone
+        if (GameObject.Find(sName + "(Clone)") != null) 
+            node = GameObject.Find(sName + "(Clone)");
+        else
+            node = GameObject.Find(sName);
+
+        return node;
+    }
+
+
+
+
+
 }
